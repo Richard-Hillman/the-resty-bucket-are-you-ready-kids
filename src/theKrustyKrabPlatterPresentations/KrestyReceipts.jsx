1939a1d9260d3fc6145c13da6440134f9723c726
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-02-07 20:10:20 -0800

    added receipts page. added container state and props.

---
+++ src/theKrustyKrabPlatterPresentations/KrestyReceipts.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const KrustyReceipts = ({ results }) => {
+  return (
+    <>
+        
+      <div>Response</div>
+
+    </>
+  );
+};
+
+KrustyReceipts.PropTypes = {
+  results: PropTypes.string.isRequired
+};
+
+export default KrustyReceipts;
